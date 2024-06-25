6131e61b87ffee1522e114146ee0f68629974435
adminshivam <shivam.ku.gupta@gmail.com>
2024-06-25 10:17:36 +0530

    Added operators in python.

---
+++ operators.py
@@ -0,0 +1,16 @@
+a = 10
+b = 20
+
+a += 1
+b -= 1
+print(a, b)
+
+a *= 2
+b **= 2
+print(a, b)
+
+if(a == 10) : print(a, b)
+if(a == 10) : 
+  print(a, b)
+
+print("Done!") if (a == 22) else print("Not Done!")
